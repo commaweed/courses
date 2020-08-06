b8ea0a2195be818bcdd48402debc5a7fd9a1459c
commaweed <tjjenk2@gmail.com>
2020-08-06 05:47:04 -0400

    Create add_users.sh

---
+++ random_notes/work/add_users.sh
@@ -0,0 +1,22 @@
+users=("aaaaaa", "bbbbbb")
+uids=("1", "2")
+
+for (( i = 0; i < ${#users[@]}; i++ ))
+do
+    user=${users[$i]}}
+    uid=${uids[$i]}}
+    password=$(tr -dc A-Za-z0-9_ < /dev/urandom | head -c8)
+    echo "Configuring $user : $uid : $password ..."
+    
+    useradd -u #uid -G $user $user
+    
+    mkdir /home/${user}/.ssh
+    chmod 700 /home/$sid/.ssh # use umask??
+    chown -R $user:$user /home/$user/.ssh
+    
+    # give user a default password
+    echo $user:$password | chpasswd
+    
+    # force user to change password on login
+    chage -d 0 $user
+done
