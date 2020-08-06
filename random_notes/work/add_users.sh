eff085bfbd3a8fa20612366d9db2a2fae459e4ae
commaweed <tjjenk2@gmail.com>
2020-08-06 05:48:34 -0400

    Update add_users.sh

--- random_notes/work/add_users.sh
+++ random_notes/work/add_users.sh
@@ -10,15 +10,15 @@ do
     password=$(tr -dc A-Za-z0-9_ < /dev/urandom | head -c8)
     echo "Configuring $user : $uid : $password ..."
     
-    useradd -u #uid -G $user $user
+    useradd -u ${uid} -G ${user} ${user}
     
     mkdir /home/${user}/.ssh
-    chmod 700 /home/$sid/.ssh # use umask??
-    chown -R $user:$user /home/$user/.ssh
+    chmod 700 /home/${user}/.ssh # use umask??
+    chown -R ${user}:${user} /home/${user}/.ssh
     
     # give user a default password
     echo $user:$password | chpasswd
     
     # force user to change password on login
-    chage -d 0 $user
+    chage -d 0 ${user}
 done
