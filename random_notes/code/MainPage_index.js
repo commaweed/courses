9e7618a5d80cb35fdd505ef69774e05043633bf4
commaweed <tjjenk2@gmail.com>
2020-09-10 07:15:29 -0400

    Update MainPage_index.js

--- random_notes/code/MainPage_index.js
+++ random_notes/code/MainPage_index.js
@@ -58,4 +58,76 @@ export default connect((store) => {
         drilldownQuery: store.drilldownQueryStore.drilldownQuery
     }
 }) (Form.create()(CSSModules(SearchForm, styles)));
+
+SearchForm/SearchForm.js:
+
+import { submitQuery } from '../../../../app-redux/actions/searchFormActions';
+import { fetchConstants } from '../../../../app-redux/actions/constantsActions';
+import { fulFillDrilldownQuery } from '../../../../app-redux/actions/drilldownQueryActions';
+import { getRecord } from '../../../../services/myService';
+
+...
+constructor(props) {
+    super(props);
+    this.state = {
+        chosenValue: getRecord("default value"),
+        searchOptionsVisibility: false,
+        drilldownQuery: null
+    };
+}
+
+componentWillMount() {
+    this.props.dispatch(fetchConstants());
+}
+
+componentDidMount() {
+    if (this.props.drilldownQuery) {
+        const form = this.props.form;
+        const { validateFields, getFieldsError } = form;
+        this.props.dispatch(fulFillDrilldownQuery());
+        this.setDrilldownFieldValues();
+        validateFields();
+        
+        if (!hasErrors(getFieldsError())) {
+            this.handleFormSubmit();
+        } else {
+        }
+    } else {
+        this.props.form.validateFields(); // disable's submit button at the beginning
+    }
+}
+
+componentWillReceiveProps(nextProps) {
+    if (this.state.chosenConstant === null && nextProps.fetchingConstants === false &&& nextProps.constants.length > 0) {
+        const form = this.props.form;
+        this.setState({
+            form.getFieldValue(FORM_FIELDS.constantsField.name) || ConstantsField.DEFAULT_VALUE
+        });
+    }
+}
+
+handleFormReset = () => {
+    this.handleConstantsChange();
+    this.props.form.resetFields();
+    this.props.form.validateFields();
+};
+
+handleFormSubmit = (event) => {
+    if (event && event.preventDefault) event.preventDefault();
+    this.props.form.validateFields((err, query) => {
+        if (!err) {
+            const requestParams = convertFormFieldValuesToRequestParams(query);
+            this.props.dispatch(submitQuery(requestParams));
+        }
+    });
+};
+
+SearchForm.propTypes = {
+    drilldownQuery: PropTypes.object
+};
+
+SearchForm.defaultProps = {
+    drilldownQuery: null
+};
+
 */
