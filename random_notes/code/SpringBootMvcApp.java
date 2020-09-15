a0ce10a55b8c4ee04a238ec22c3077bab4235df3
commaweed <tjjenk2@gmail.com>
2020-09-15 04:40:12 -0400

    Create SpringBootMvcApp.java

---
+++ random_notes/code/SpringBootMvcApp.java
@@ -0,0 +1,12 @@
+@SpringBootApplication(scanBasePackages="my.package")
+@EnableScheduling
+public class MyApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyApplication.class);
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
